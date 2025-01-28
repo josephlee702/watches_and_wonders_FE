72169d613857f9db7d6729d761ccb74062aabf80
Joseph Lee <jhjlee702@gmail.com>
2025-01-27 17:08:47 -0700

    define routes for the front end app and assign components to each URL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// 'react-router-dom' is a library that handles routing in React applications
+// "BrowserRouter" is renamed as "Router"
+// "Route" is used to define individual routes in my app
+// "Routes" is used to wrap all Route components. It handles the routing and decides which component to render based on the current URL.
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// imports BrandIndex component from BrandIndex.js , which will be rendered when the URL matches the /brands route
+import BrandIndex from './pages/BrandIndex';
+// same as above, just for WatchIndex component and /watches route
+import WatchIndex from './pages/WatchIndex';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* each Route maps a specific URL to a component that should be displayed when that URL is visited */}
+        <Route path="/brands" element={<BrandIndex />} />
+        <Route path="/watches" element={<WatchIndex />} />
+      </Routes>
+    </Router>
   );
 }
 
